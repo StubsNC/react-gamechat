a48504d69b41e4d27d5d8c8fa8261f9ec8937e2b
Stubs <stubblefieldjh@g.cofc.edu>
2023-03-17 21:30:42 -0400

    Styled RoomPage

--- src/components/ChatRoomComponents/RoomPage.js
+++ src/components/ChatRoomComponents/RoomPage.js
@@ -1,12 +1,14 @@
 import React, { useState, useRef } from "react";
 import { Auth } from "./Auth";
-import {  Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { Container, Button } from "../../globalStyles";
-
+import { RoomSec, RoomRow, RoomColumn, RoomForm, RoomContainer, RoomSubHeading, RoomSubText, RoomComponent } from './RoomPage.elements'
+import { FooterSubHeading, FooterSubscription, FooterSubText } from "../Footer/Footer.elements";
 
 
 import Cookies from 'universal-cookie';
 import ChatRoom from "./Chatroom";
+import { FormInput } from "../Footer/Footer.elements";
 const cookies = new Cookies()
 
 
@@ -28,25 +30,24 @@ function Room() {
       {room ? (
         <ChatRoom room={room} />
       ) : (
-        <Container >
-          <div>
-            <Row>
-              <Col>
-                <label>Enter Room Name:</label>
-              </Col>
-              <Col xs={8}>
-                <div >
-                  <input type="text" ref={roomInputRef} />
-                  <div >
-                    <Button type="button" onClick={() => setRoom(roomInputRef.current.value)}>
-                      Enter Chat
-                    </Button>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Container>
+        <RoomSec>
+          <Container >
+            <RoomContainer>
+              <RoomComponent>
+                <RoomSubHeading>
+                  Join our exclusive membership to receive the latest news and trends
+                </RoomSubHeading>
+                <RoomSubText>Remember to be careful of who you share your room key with!</RoomSubText>
+                <RoomForm >
+                  <FormInput type="text" ref={roomInputRef} />
+                  <Button fontBig type="button" onClick={() => setRoom(roomInputRef.current.value)}>
+                    Enter Chat
+                  </Button>
+                </RoomForm>
+              </RoomComponent>
+            </RoomContainer>
+          </Container>
+        </RoomSec>
       )}
     </>
 
